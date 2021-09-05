52b1a8a1fe63a8179cd472713de69e3c7167867d
Omar Hasan <omar.hassan3030@gmail.com>
2021-09-05 22:44:48 +0300

    feat(public): add shared components

--- apps/user/src/app/app.module.ts
+++ apps/user/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/pages/home/home.component';
+import { AboutComponent } from './core/pages/about/about.component';
+import { HeaderComponent } from './core/shared/components/header/header.component';
+import { FooterComponent } from './core/shared/components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
   providers: [],
   bootstrap: [AppComponent],
