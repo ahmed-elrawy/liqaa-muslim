28ff7bfbc8549aa2e3b852b7b2d17b7ada6b643e
Omar Hasan <omar.hassan3030@gmail.com>
2021-09-05 23:24:24 +0300

    fix(shared):  refine the path of shared

--- apps/user/src/app/app.module.ts
+++ apps/user/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/pages/home/home.component';
 import { AboutComponent } from './core/pages/about/about.component';
-import { HeaderComponent } from './core/shared/components/header/header.component';
-import { FooterComponent } from './core/shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent, HeaderComponent, FooterComponent],
