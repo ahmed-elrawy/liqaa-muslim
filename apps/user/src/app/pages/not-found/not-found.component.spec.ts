716b5a4400cd5d4fbc410a0d5faaefe59073c209
Ahmed Sokar <71194768+Ahmed-Sokar2020@users.noreply.github.com>
2021-09-07 23:03:25 +0200

    fix(notfound): initiate not found component

--- apps/user/src/app/pages/not-found/not-found.component.spec.ts
+++ apps/user/src/app/pages/not-found/not-found.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PageNotfoundComponent } from './page-notfound.component';
+import { NotFoundComponent } from './not-found.component';
 
-describe('PageNotfoundComponent', () => {
-  let component: PageNotfoundComponent;
-  let fixture: ComponentFixture<PageNotfoundComponent>;
+describe('NotFoundComponent', () => {
+  let component: NotFoundComponent;
+  let fixture: ComponentFixture<NotFoundComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PageNotfoundComponent ]
+      declarations: [ NotFoundComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PageNotfoundComponent);
+    fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
