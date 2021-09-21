79c6e589c43ee6af4250a30d67ecba0725649550
Ahmed Sokar <71194768+Ahmed-Sokar2020@users.noreply.github.com>
2021-09-22 01:42:27 +0200

    fix(second commit): this is my second commit

--- apps/user/src/app/app.module.ts
+++ apps/user/src/app/app.module.ts
@@ -6,11 +6,16 @@ import { HomeComponent } from './core/pages/home/home.component';
 import { AboutComponent } from './core/pages/about/about.component';
 import { HeaderComponent } from './core/shared/components/header/header.component';
 import { FooterComponent } from './core/shared/components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
-  providers: [],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule
+],
+  providers: [DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ apps/user/src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'ng-arab-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  open:boolean=false
+  constructor() {
+
+  }
+  classForm=new FormGroup({
+      age:new FormControl(null,[Validators.required,Validators.max(100)]),
+      gender:new FormControl(null,[Validators.required])
+  })
+  ngOnInit(): void {
+  }
+
+}
