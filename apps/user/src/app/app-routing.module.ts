254ab6d5f29329470e8a5ba3c7ff9ac85d2e0e95
Ahmed Sokar <71194768+Ahmed-Sokar2020@users.noreply.github.com>
2021-09-22 21:55:59 +0200

    fix(components): refine many components

---
+++ apps/user/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from './pages/about/about.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+
+  },
+  {
+    path: 'home' ,
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,
+    {initialNavigation: 'enabledBlocking'})
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- apps/user/src/app/app.module.ts
+++ apps/user/src/app/app.module.ts
@@ -1,21 +1,20 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './core/pages/home/home.component';
-import { AboutComponent } from './core/pages/about/about.component';
-import { HeaderComponent } from './core/shared/components/header/header.component';
-import { FooterComponent } from './core/shared/components/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
-import { DatePipe } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    HttpClientModule
-],
-  providers: [DatePipe],
+    AppRoutingModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
