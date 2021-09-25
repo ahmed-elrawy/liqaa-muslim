e91ecd0770da0235ecd1989f87c42021dc0f5ffc
hussien10 <32900347+hussien10@users.noreply.github.com>
2021-09-25 21:00:20 +0200

    feat(home): intialize home page

--- apps/user/src/app/app.module.ts
+++ apps/user/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './core/pages/home/home.component';
-import { AboutComponent } from './core/pages/about/about.component';
-import { HeaderComponent } from './core/shared/components/header/header.component';
-import { FooterComponent } from './core/shared/components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
 
@@ -14,6 +15,7 @@ import { DatePipe } from '@angular/common';
   declarations: [AppComponent, HomeComponent, AboutComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    RouterModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule
